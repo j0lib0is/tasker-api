f764a9cd96310f25815782f96833cd1e3d403a5a
jacobjolibois <jacobjolibois@hey.com>
2022-02-21 08:54:10 -0600

    set up password hashing and decryption

---
+++ api/users/users-model.js
@@ -0,0 +1,17 @@
+const db = require('../../data/db-config');
+
+// Models
+function getById(id) {
+
+}
+
+function update(id, payload) {
+
+}
+
+// Exports
+module.exports = {
+	getById,
+	create,
+	update
+};
