c9be3a407fc7c2604807cb8ffc00fe879a868fd9
jacobjolibois <jacobjolibois@hey.com>
2022-03-07 17:27:35 -0600

    set up database migration and rollback for prod and dev

--- api/server.js
+++ api/server.js
@@ -12,26 +12,20 @@ const listsRouter = require('./lists/lists-router');
 const server = express();
 server.use(express.json());
 
-// Session Config
-const sessionConfig = {
-  name: 'theGreatestSession',
-  secret: 'the super-secret secret',
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24,
-    secure: false,
-    httpOnly: true,
-  },
-  resave: false,
-  saveUninitialized: false
-};
-
-server.use(session(sessionConfig));
-
 // Routers
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 server.use('/api/tasks', taskRouter);
 server.use('/api/lists', listsRouter);
+
+// Global error handler
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    customError: 'Houston, we have a problem.',
+    message: err.message,
+    stack: err.stack
+  });
+});
 
 // Exports
 module.exports = server;
